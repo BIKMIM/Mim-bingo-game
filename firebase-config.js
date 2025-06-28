e984df2b2ff7e400aa490f6d57cbb298d794e640
BIKMIM <38391133+BIKMIM@users.noreply.github.com>
2025-06-28 20:39:58 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase 설정
+const firebaseConfig = {
+    apiKey: "AIzaSyAqHKfUCypD_Jk7mXgH5u-O1XS7Dje4DZ8",
+    authDomain: "mim-bingo-game.firebaseapp.com",
+    databaseURL: "https://mim-bingo-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mim-bingo-game",
+    storageBucket: "mim-bingo-game.firebasestorage.app",
+    messagingSenderId: "436254307510",
+    appId: "1:436254307510:web:23ad7c9a0d10d8ad26217"
+};
+
+// Firebase 초기화 (script.js에서 초기화되도록 내보냅니다)
+// firebase.initializeApp(firebaseConfig);
+// const database = firebase.database();
+// const auth = firebase.auth();
